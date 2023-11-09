adc566f7dec9faa95a5c441517b886e643cbc565
nathanhalgumacoisa <nathan.henrique@aluno.senai.br>
2023-11-09 14:59:49 -0300

    create server

---
+++ src/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { config } from "dotenv";
+
+config();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+
+    res.status(200).json({message: "Hello World!"});
+})
+
+app.listen(port, () => {
+    console.log('sever started on http://localhost:${port}')
+})
